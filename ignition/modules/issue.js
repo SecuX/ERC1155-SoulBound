87fa32ecbe2aa3e4c601bdf8dcfd62cd8c29b8c0
rus7hex <dick525724@gmail.com>
2024-05-30 11:23:26 +0800

    change ignition target address to smart contract address (fit usecase)

--- ignition/modules/issue.js
+++ ignition/modules/issue.js
@@ -3,7 +3,7 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 module.exports = {
     default: buildModule(`sx${Date.now()}`, (m) => {
         const sbt = m.contractAt("ERC1155SoulBound", process.env.CONTRACT_ADDRESS);
-        m.call(sbt, "issue", ["0xC4DD028e4f646ba9AEDF5394C88082983a6e5801", 0, 1],
+        m.call(sbt, "issue", ["0xa4D19bF15601c0641ADC62Cc389b24C450cF918F", 0, 1],
             {
                 from: {
                     type: "ACCOUNT",
